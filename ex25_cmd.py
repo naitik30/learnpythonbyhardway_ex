257a4b22856f557f8169b04f32b043dc6bfec893
naitik <patelnaitik30@gmail.com>
2017-05-14 13:07:50 -0700

    completed 33 exec.

---
+++ ex25_cmd.py
@@ -0,0 +1,24 @@
+import ex25
+
+sentence = "All good things come to those who wait."
+
+words = ex25.break_words(sentence)
+
+print words
+
+sorted_words = ex25.sort_words(words)
+
+print sort_words
+
+print ex25.print_first_word(words)
+
+print ex25.print_last_word(words)
+
+print words
+
+print ex25.print_first_word(sorted_words)
+
+print ex25.print_last_word(sorted_words)
+
+print sorted_words
+
